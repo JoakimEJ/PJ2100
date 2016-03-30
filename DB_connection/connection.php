b803d9eee28d77fb0bf30d54776c2f657bf5bce6
Joakim E Jacobsen <JoakimEJacobsen@gmail.com>
2016-03-30 11:05:01 +0200

    Added arbeidskontrakt to MSF-folder, + a database connection-script in php

---
+++ DB_connection/connection.php
@@ -0,0 +1,27 @@
+<?php
+	$server = "localhost";
+	$username = "root";
+	$pw = "AliasAsics123";
+	$db = "test";
+
+	// Create connection
+	$conn = mysqli_connect($server, $username, $pw, $db);
+
+	// Check connection
+	if ($conn->connect_error){
+		die("Connection failed: " . $conn->connect_error);
+	}
+	else {
+		echo "Connected successfully <br/>";
+	}
+
+	$query = "SELECT * FROM Brukere";
+
+	$result = mysqli_query($conn, $query);
+
+	while($person = mysqli_fetch_array($result)){
+		echo $person['Navn'] . "<br/>";
+	}
+	echo "Ferdig!";
+
+?>
